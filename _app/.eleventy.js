b528f1f201b629395f9f3d6ea4ebd0a916d1e5bd
Charlie Owen <charlie@sonniesedge.co.uk>
2020-08-26 18:40:15 +0200

    Change readmes to index files

--- _app/.eleventy.js
+++ _app/.eleventy.js
@@ -1,6 +1,16 @@
-module.exports = {
-    dir: {
-      input: "../",
-      output: "dist"
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addCollection('readme', collection => {
+        return collection.getAllSorted().map(item => {
+            if (item.fileSlug === 'README') {
+                item.outputPath = item.outputPath.replace('/README', '');
+            }
+        });
+    });
+
+    return {
+        dir: {
+            input: "../",
+            output: "dist"
+        }
     }
   };
